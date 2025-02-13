e084b9af5c89fe2349883b0d55f62c2d50bba5bc
2ha2ha <30305964@qq.com>
2025-02-14 02:59:01 +0800

    定义 dev_const

--- QLinkLib.c
+++ QLinkLib.c
@@ -11,6 +11,7 @@
 
 #define MEM_READ_SIZE 1024
 
+extern struct stlink_chipid_params dev_const[];
 struct stlink_chipid_params *stlink_chipid_get_params_hard(uint32_t chip_id) {
     for(uint32_t i=0; i<sizeof(dev_const)/sizeof(dev_const[0]); i++) {
         if(dev_const[i].chip_id == chip_id) {
