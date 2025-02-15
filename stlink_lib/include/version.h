fd11f0d623097b5f0a703c9e027a965cbd03e4b6
2ha2ha <30305964@qq.com>
2025-02-15 20:43:42 +0800

    添加新编译的 stlink_lib

--- stlink_lib/include/version.h
+++ stlink_lib/include/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define STLINK_VERSION       "1.8.0-91-g9446bf5"
+#define STLINK_VERSION       "1.8.0-91-g9446bf5-dirty"
 #define STLINK_VERSION_MAJOR 1
 #define STLINK_VERSION_MINOR 8
 #define STLINK_VERSION_PATCH 0
